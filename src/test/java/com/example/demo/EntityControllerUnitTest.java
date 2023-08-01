bbc8be309722b9ceca73663021e26bae10edaa13
jesusfdezGallardo <jesusfgz91@gmail.com>
2023-08-02 00:34:04 +0200

    Prueba de nivel Accenture

--- src/test/java/com/example/demo/EntityControllerUnitTest.java
+++ src/test/java/com/example/demo/EntityControllerUnitTest.java
@@ -1,6 +1,7 @@
 
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
@@ -19,11 +20,14 @@ import java.util.Optional;
 import java.time.LocalDateTime;
 import java.time.format.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -34,68 +38,278 @@ import com.example.demo.entities.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-
-/** TODO
- * Implement all the unit test in its corresponding class.
- * Make sure to be as exhaustive as possible. Coverage is checked ;)
- */
-
 @WebMvcTest(DoctorController.class)
-class DoctorControllerUnitTest{
+class DoctorControllerUnitTest {
 
     @MockBean
     private DoctorRepository doctorRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    void shouldGetAllDoctorsTest() throws Exception {
+        List<Doctor> doctorList = new ArrayList<>();
+        Doctor doctor = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        doctorList.add(doctor);
+        when(doctorRepository.findAll()).thenReturn(doctorList);
+        mockMvc.perform(get("/api/doctors").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(doctorList)))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void errorGetAllDoctorsTest() throws Exception {
+        List<Doctor> doctorList = new ArrayList<>();
+        when(doctorRepository.findAll()).thenReturn(doctorList);
+        mockMvc.perform(get("/api/doctors").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(doctorList)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void getDoctorByIdOkTest() throws Exception {
+        Doctor doctor = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        doctor.setId(1);
+        Optional<Doctor> opt = Optional.of(doctor);
+
+        when(doctorRepository.findById(Mockito.any())).thenReturn(opt);
+        mockMvc.perform(get("/api/doctors/" + doctor.getId()).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(doctor)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldNotGetDoctorById() throws Exception {
+        long doctorId = 1;
+        when(doctorRepository.findById(doctorId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/api/doctors/" + doctorId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldCreateDoctor() throws Exception {
+        Doctor doctor = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        when(doctorRepository.save(Mockito.any())).thenReturn(doctor);
+        mockMvc.perform(post("/api/doctor").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(doctor)))
+                .andExpect(status().isCreated());
+    }
+    @Test
+    void shouldDeleteDoctorById() throws Exception {
+        Doctor doctor = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        doctor.setId(1);
+        Optional<Doctor> opt = Optional.of(doctor);
+        assertThat(opt).isPresent();
+        assertThat(opt.get().getId()).isEqualTo(doctor.getId());
+        assertThat(doctor.getId()).isEqualTo(1);
+        when(doctorRepository.findById(Mockito.any())).thenReturn(opt);
+        mockMvc.perform(delete("/api/doctors/"+doctor.getId()))
+                .andExpect(status().isOk());
+    }
+    @Test
+    void shouldNopDeleteDoctorById() throws Exception {
+        long doctorId = 1;
+        mockMvc.perform(delete("/api/doctors/"+doctorId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldDeleteAllDoctors () throws Exception{
+        mockMvc.perform(delete("/api/doctors"))
+                .andExpect(status().isOk());
     }
 }
 
 
 @WebMvcTest(PatientController.class)
-class PatientControllerUnitTest{
+class PatientControllerUnitTest {
 
     @MockBean
     private PatientRepository patientRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    void shouldGetAllPatientsTest() throws Exception {
+        List<Patient> patients = new ArrayList<>();
+        Patient patient1 = new Patient("Juan","Carlos", 34, "j.carlos@hospital.accwe");
+        patients.add(patient1);
+        Patient patient2 = new Patient("Cornelio","Andrea", 59, "c.andrea@hospital.accwe");
+        patients.add(patient2);
+        Patient patient3 = new Patient("Clarisa","Julia", 29, "c.julia@hospital.accwe");
+        patients.add(patient3);
+
+        when(patientRepository.findAll()).thenReturn(patients);
+        mockMvc.perform(get("/api/patients").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patients)))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void errorGetAllPatientsTest() throws Exception {
+        List<Patient> patientList = new ArrayList<>();
+        when(patientRepository.findAll()).thenReturn(patientList);
+        mockMvc.perform(get("/api/patients").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patientList)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void getPatientByIdOkTest() throws Exception {
+        Patient patient = new Patient("Juan","Carlos", 34, "j.carlos@hospital.accwe");
+        patient.setId(1);
+        Optional<Patient> opt = Optional.of(patient);
+
+        when(patientRepository.findById(Mockito.any())).thenReturn(opt);
+        mockMvc.perform(get("/api/patients/" + patient.getId()).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patient)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldNotGetPatientById() throws Exception {
+        long doctorId = 1;
+        when(patientRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+        mockMvc.perform(get("/api/patients/" + doctorId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldCreatePatient() throws Exception {
+        Patient patient = new Patient("Juan","Carlos", 34, "j.carlos@hospital.accwe");
+        when(patientRepository.save(Mockito.any())).thenReturn(patient);
+        mockMvc.perform(post("/api/patient").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patient)))
+                .andExpect(status().isCreated());
+    }
+    @Test
+    void shouldDeletePatientById() throws Exception {
+        Patient patient = new Patient("Juan","Carlos", 34, "j.carlos@hospital.accwe");
+        patient.setId(1);
+        Optional<Patient> opt = Optional.of(patient);
+        assertThat(opt).isPresent();
+        assertThat(opt.get().getId()).isEqualTo(patient.getId());
+        assertThat(patient.getId()).isEqualTo(1);
+        when(patientRepository.findById(Mockito.any())).thenReturn(opt);
+        mockMvc.perform(delete("/api/patients/"+patient.getId()))
+                .andExpect(status().isOk());
+    }
+    @Test
+    void shouldNopDeletePatientById() throws Exception {
+        long doctorId = 1;
+        mockMvc.perform(delete("/api/patients/"+doctorId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldDeleteAllDoctors () throws Exception{
+        mockMvc.perform(delete("/api/patients"))
+                .andExpect(status().isOk());
     }
 
 }
 
 @WebMvcTest(RoomController.class)
-class RoomControllerUnitTest{
+class RoomControllerUnitTest {
 
     @MockBean
     private RoomRepository roomRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    void shouldGetAllRoomsTest() throws Exception {
+        List<Room> rooms = new ArrayList<>();
+        Room room1 = new Room("Dermatology");
+        rooms.add(room1);
+        Room room2 = new Room("Operations");
+        rooms.add(room2);
+        Room room3 = new Room("Emergencies");
+        rooms.add(room3);
+
+        when(roomRepository.findAll()).thenReturn(rooms);
+        mockMvc.perform(get("/api/rooms").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(rooms)))
+                .andExpect(status().isOk());
+
     }
+
+    @Test
+    void errorGetAllRoomsTest() throws Exception {
+        List<Room> roomList = new ArrayList<>();
+        when(roomRepository.findAll()).thenReturn(roomList);
+        mockMvc.perform(get("/api/rooms").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(roomList)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void getRoomByIdOkTest() throws Exception {
+        Room room = new Room("Dermatology");
+        Optional<Room> opt = Optional.of(room);
+
+        when(roomRepository.findByRoomName(Mockito.any())).thenReturn(opt);
+        mockMvc.perform(get("/api/rooms/" + room.getRoomName()).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(room)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldNotGetRoomById() throws Exception {
+        long doctorId = 1;
+        when(roomRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+        mockMvc.perform(get("/api/rooms/" + doctorId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldCreateRoom() throws Exception {
+        Room room = new Room("Dermatology");
+        when(roomRepository.save(Mockito.any())).thenReturn(room);
+        mockMvc.perform(post("/api/room").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(room)))
+                .andExpect(status().isCreated());
+    }
+    @Test
+    void shouldDeleteRoomById() throws Exception {
+        Room room = new Room("Dermatology");
+        Optional<Room> opt = Optional.of(room);
+        assertThat(opt).isPresent();
+        assertThat(opt.get().getRoomName()).isEqualTo(room.getRoomName());
+        when(roomRepository.findByRoomName(Mockito.any())).thenReturn(opt);
+        mockMvc.perform(delete("/api/rooms/"+room.getRoomName()))
+                .andExpect(status().isOk());
+    }
+    @Test
+    void shouldNopDeleteRoomById() throws Exception {
+        String roomsName = "Test";
+        mockMvc.perform(delete("/api/rooms/"+roomsName))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldDeleteAllRooms () throws Exception{
+        mockMvc.perform(delete("/api/rooms"))
+                .andExpect(status().isOk());
+    }
+
 
 }
--- src/test/java/com/example/demo/EntityUnitTest.java
+++ src/test/java/com/example/demo/EntityUnitTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -20,16 +21,16 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import com.example.demo.entities.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace=Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @TestInstance(Lifecycle.PER_CLASS)
 class EntityUnitTest {
 
-	@Autowired
-	private TestEntityManager entityManager;
+    @Autowired
+    private TestEntityManager entityManager;
 
-	private Doctor d1;
+    private Doctor d1;
 
-	private Patient p1;
+    private Patient p1;
 
     private Room r1;
 
@@ -37,14 +38,278 @@ class EntityUnitTest {
     private Appointment a2;
     private Appointment a3;
 
-    @Test
-    void this_is_a_test(){
-        // DELETE THIS TEST
-        assertThat(false).isEqualTo(true);
+    @BeforeAll
+    void setUp() {
+        d1 = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        p1 = new Patient("Juan", "Carlos", 34, "j.carlos@hospital.accwe");
+        r1 = new Room("Dermatology");
+        a1 = new Appointment(p1, d1, r1,
+                LocalDateTime.parse("19:30 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")),
+                LocalDateTime.parse("20:30 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")));
+        a2 = new Appointment(p1, d1, r1,
+                LocalDateTime.parse("19:30 25/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")),
+                LocalDateTime.parse("20:30 25/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")));
+        a3 = new Appointment(p1, d1, r1,
+                LocalDateTime.parse("19:30 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")),
+                LocalDateTime.parse("20:30 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")));
     }
 
-    /** TODO
-     * Implement tests for each Entity class: Doctor, Patient, Room and Appointment.
-     * Make sure you are as exhaustive as possible. Coverage is checked ;)
-     */
+    @Test
+    void testSaveDoctor() {
+        d1 = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+
+        Doctor savedDoctor = entityManager.persistAndFlush(d1);
+        entityManager.clear();
+
+        Doctor foundDoctor = entityManager.find(Doctor.class, savedDoctor.getId());
+        assertThat(foundDoctor.getFirstName()).isEqualTo(d1.getFirstName());
+    }
+
+
+    @Test
+    void testFindDoctorById() {
+        d1 = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        Doctor savedDoctor = entityManager.persistAndFlush(d1);
+        entityManager.clear();
+
+        Optional<Doctor> foundDoctorOptional = Optional.ofNullable(entityManager.find(Doctor.class, savedDoctor.getId()));
+        assertThat(foundDoctorOptional).isPresent();
+
+        Doctor foundDoctor = foundDoctorOptional.get();
+        assertThat(foundDoctor.getId()).isEqualTo(savedDoctor.getId());
+        assertThat(foundDoctor.getFirstName()).isEqualTo("Perla");
+        assertThat(foundDoctor.getLastName()).isEqualTo("Amalia");
+        assertThat(foundDoctor.getAge()).isEqualTo(24);
+        assertThat(foundDoctor.getEmail()).isEqualTo("p.amalia@hospital.accwe");
+    }
+
+    @Test
+    void testUpdateDoctor() {
+        d1 = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        Doctor savedDoctor = entityManager.persistAndFlush(d1);
+        entityManager.clear();
+
+        Doctor foundDoctor = entityManager.find(Doctor.class, savedDoctor.getId());
+        foundDoctor.setFirstName("PruebaTest");
+        entityManager.flush();
+        entityManager.clear();
+
+        Doctor updatedDoctor = entityManager.find(Doctor.class, savedDoctor.getId());
+        assertThat(updatedDoctor.getFirstName()).isEqualTo("PruebaTest");
+    }
+
+    @Test
+    void testDeleteDoctor() {
+        d1 = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        Doctor savedDoctor = entityManager.persistAndFlush(d1);
+
+        entityManager.remove(savedDoctor);
+        entityManager.flush();
+
+        Doctor deletedDoctor = entityManager.find(Doctor.class, savedDoctor.getId());
+        assertThat(deletedDoctor).isNull();
+    }
+
+    @Test
+    void testSavePatient() {
+        p1 = new Patient("Juan", "Carlos", 34, "j.carlos@hospital.accwe");
+
+        Patient savedPatient = entityManager.persistAndFlush(p1);
+        entityManager.clear();
+
+        Patient foundPatient = entityManager.find(Patient.class, savedPatient.getId());
+        assertThat(foundPatient.getFirstName()).isEqualTo(savedPatient.getFirstName());
+    }
+
+    @Test
+    void testFindPatientById() {
+        p1 = new Patient("Juan", "Carlos", 34, "j.carlos@hospital.accwe");
+        Patient savedPatient = entityManager.persistAndFlush(p1);
+        entityManager.clear();
+
+        Optional<Patient> foundPatientOptional = Optional.ofNullable(entityManager.find(Patient.class, savedPatient.getId()));
+        assertThat(foundPatientOptional).isPresent();
+
+        Patient foundPatient = foundPatientOptional.get();
+        assertThat(foundPatient.getId()).isEqualTo(savedPatient.getId());
+        assertThat(foundPatient.getFirstName()).isEqualTo("Juan");
+        assertThat(foundPatient.getLastName()).isEqualTo("Carlos");
+        assertThat(foundPatient.getAge()).isEqualTo(34);
+        assertThat(foundPatient.getEmail()).isEqualTo("j.carlos@hospital.accwe");
+    }
+
+    @Test
+    void testUpdatePatient() {
+        p1 = new Patient("Juan", "Carlos", 34, "j.carlos@hospital.accwe");
+        Patient savedPatient = entityManager.persistAndFlush(p1);
+        entityManager.clear();
+
+        Patient foundPatient = entityManager.find(Patient.class, savedPatient.getId());
+        foundPatient.setFirstName("Juan");
+        entityManager.flush();
+        entityManager.clear();
+
+        Patient updatedPatient = entityManager.find(Patient.class, savedPatient.getId());
+        assertThat(updatedPatient.getFirstName()).isEqualTo("Juan");
+    }
+
+    @Test
+    void testDeletePatient() {
+        p1 = new Patient("Juan", "Carlos", 34, "j.carlos@hospital.accwe");
+        Patient savedPatient = entityManager.persistAndFlush(p1);
+
+        entityManager.remove(savedPatient);
+        entityManager.flush();
+
+        Patient deletedPatient = entityManager.find(Patient.class, savedPatient.getId());
+        assertThat(deletedPatient).isNull();
+    }
+
+    @Test
+    void testSaveRoom() {
+        r1 = new Room("Dermatology");
+
+        Room savedRoom = entityManager.persistAndFlush(r1);
+        entityManager.clear();
+
+        Room foundRoom = entityManager.find(Room.class, savedRoom.getRoomName());
+        assertThat(foundRoom.getRoomName()).isEqualTo(savedRoom.getRoomName());
+    }
+
+    @Test
+    void testFindRoomByName() {
+        r1 = new Room("Dermatology");
+        Room savedRoom = entityManager.persistAndFlush(r1);
+        entityManager.clear();
+
+        Optional<Room> foundRoomOptional = Optional.ofNullable(entityManager.find(Room.class, savedRoom.getRoomName()));
+        assertThat(foundRoomOptional).isPresent();
+
+        Room foundRoom = foundRoomOptional.get();
+        assertThat(foundRoom.getRoomName()).isEqualTo(savedRoom.getRoomName());
+        assertThat(foundRoom.getRoomName()).isEqualTo("Dermatology");
+    }
+
+    @Test
+    void testUpdateRoom() {
+        r1 = new Room("Dermatology");
+        Room savedRoom = entityManager.persistAndFlush(r1);
+        entityManager.clear();
+
+        Room foundRoom = entityManager.find(Room.class, savedRoom.getRoomName());
+        entityManager.clear();
+
+        Room updatedRoom = entityManager.find(Room.class, savedRoom.getRoomName());
+        assertThat(updatedRoom.getRoomName()).isEqualTo("Dermatology");
+    }
+
+    @Test
+    void testDeleteRoom() {
+        r1 = new Room("Dermatology");
+        Room savedRoom = entityManager.persistAndFlush(r1);
+
+        entityManager.remove(savedRoom);
+
+        Room deletedRoom = entityManager.find(Room.class, savedRoom.getRoomName());
+        assertThat(deletedRoom).isNull();
+    }
+
+    @Test
+    void testSaveAppointment() {
+        Appointment savedAppointment = entityManager.persistAndFlush(a1);
+
+        Appointment foundAppointment = entityManager.find(Appointment.class, savedAppointment.getId());
+        assertThat(foundAppointment).isEqualTo(savedAppointment);
+    }
+
+    @Test
+    void testFindAppointmentById() {
+        // Creamos las entidades relacionadas
+        Doctor doctor = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        Patient patient = new Patient("Juan", "Carlos", 34, "j.carlos@hospital.accwe");
+        Room room = new Room("Dermatology");
+
+        // Persistimos las entidades relacionadas para asegurarnos de que estén en estado "managed"
+        entityManager.persistAndFlush(doctor);
+        entityManager.persistAndFlush(patient);
+        entityManager.persistAndFlush(room);
+
+        // Creamos la entidad Appointment con las entidades relacionadas ya persistidas
+        Appointment appointment = new Appointment(patient, doctor, room,
+                LocalDateTime.parse("19:30 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")),
+                LocalDateTime.parse("20:30 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")));
+
+        // Persistimos la entidad Appointment
+        entityManager.persistAndFlush(appointment);
+        long appointmentId = appointment.getId();
+
+        // Limpiamos el contexto de persistencia para asegurarnos de que todas las entidades estén desvinculadas
+        entityManager.clear();
+
+        // Buscamos la entidad Appointment por su ID
+        Optional<Appointment> foundAppointmentOptional = Optional.ofNullable(entityManager.find(Appointment.class, appointmentId));
+        assertThat(foundAppointmentOptional).isPresent();
+
+        // Obtenemos la entidad encontrada
+        Appointment foundAppointment = foundAppointmentOptional.get();
+
+        // Verificamos que los atributos de la entidad encontrada coincidan con los de la entidad original
+        assertThat(foundAppointment.getId()).isEqualTo(appointment.getId());
+        assertThat(foundAppointment.getStartsAt()).isEqualTo(appointment.getStartsAt());
+        assertThat(foundAppointment.getFinishesAt()).isEqualTo(appointment.getFinishesAt());
+        assertThat(foundAppointment.getPatient().getFirstName()).isEqualTo(appointment.getPatient().getFirstName());
+        assertThat(foundAppointment.getDoctor().getFirstName()).isEqualTo(appointment.getDoctor().getFirstName());
+        assertThat(foundAppointment.getRoom().getRoomName()).isEqualTo(appointment.getRoom().getRoomName());
+    }
+
+    @Test
+    void testOverlaps() {
+        Appointment overlappingAppointment = new Appointment(p1, d1, r1,
+                LocalDateTime.parse("19:00 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")),
+                LocalDateTime.parse("20:00 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")));
+
+        boolean isOverlapping = a1.overlaps(overlappingAppointment);
+        assertThat(isOverlapping).isTrue();
+    }
+
+    @Test
+    void testNotOverlapping() {
+        // Create an appointment that does not overlap with the original appointment
+        Appointment nonOverlappingAppointment = new Appointment(p1, d1, r1,
+                LocalDateTime.parse("17:00 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")),
+                LocalDateTime.parse("18:00 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")));
+
+        boolean isOverlapping = a1.overlaps(nonOverlappingAppointment);
+        assertThat(isOverlapping).isFalse();
+    }
+
+    @Test
+    void testDeleteAppointment() {
+        // Creamos las entidades relacionadas
+        Doctor doctor = new Doctor("Perla", "Amalia", 24, "p.amalia@hospital.accwe");
+        Patient patient = new Patient("Juan", "Carlos", 34, "j.carlos@hospital.accwe");
+        Room room = new Room("Dermatology");
+
+        // Persistimos las entidades relacionadas para asegurarnos de que estén en estado "managed"
+        entityManager.persistAndFlush(doctor);
+        entityManager.persistAndFlush(patient);
+        entityManager.persistAndFlush(room);
+
+        // Creamos la entidad Appointment con las entidades relacionadas ya persistidas
+        Appointment appointment = new Appointment(patient, doctor, room,
+                LocalDateTime.parse("19:30 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")),
+                LocalDateTime.parse("20:30 24/04/2023", DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")));
+
+        // Persistimos la entidad Appointment
+        entityManager.persistAndFlush(appointment);
+        long appointmentId = appointment.getId();
+
+        // Removemos la entidad usando su ID
+        entityManager.remove(entityManager.find(Appointment.class, appointmentId));
+        entityManager.flush();
+
+        // Verificamos que la entidad fue eliminada
+        Appointment deletedAppointment = entityManager.find(Appointment.class, appointmentId);
+        assertThat(deletedAppointment).isNull();
+    }
 }
